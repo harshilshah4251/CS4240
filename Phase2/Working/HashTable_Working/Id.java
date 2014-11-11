9f13523f3425acb51d33144c18f1c31e3d599342
Kyu <tkim371@gatech.edu>
2014-11-11 01:45:28 -0500

    comparing Expr

---
+++ Phase2/Working/HashTable_Working/Id.java
@@ -0,0 +1,38 @@
+//
+//  Id.java
+//  4240
+//
+//  Created by Sung Hye Jeon on 11/3/14.
+//  Copyright (c) 2014 Sung Hye Jeon. All rights reserved.
+//
+
+
+public class Id extends Expr {
+
+	public int offset;
+	public boolean init;
+	// not sure what's offset is for
+
+	public Id(String id, Type p, int b) { 
+		super(id,p); offset = b; 
+		}
+
+	public Id(String id, Type p, boolean i) {
+		super(id,p);
+		init = i;
+	}
+	public Id(String id, Type p) {
+		super(id,p);
+		init = false;
+	}
+
+	public void initId() {
+		init = true;
+	}
+		
+
+	public String toString() {
+		return (" ID/"+  type + ", init: " + init+ ", size: " + type.size +"\n");
+	}
+
+}
---
+++ Phase2/Working/HashTable_Working/UserType.java
@@ -0,0 +1,42 @@
+public class UserType extends Type {
+	public Type of;
+	public String userTypeName;
+
+	// Should check here agin
+	// Should check here agin
+	public UserType(String s) {
+		super(s);
+		/*if(s.equals("int")) {
+			size = 4;
+		}*/
+	}
+	
+	public UserType(String n, Type p) {
+		super(p.name,p.size);
+		userTypeName = n;
+		of = p;
+	}
+	/*public UserType(String n, String s, Type p) {
+		super(s, p.size);
+		userTypeName = n;
+	}*/
+
+    public UserType(String s, int w, Type p) {
+    // check size again
+        super(s, w*p.size);
+
+    // Check size Again
+        size = w;
+        of = p;
+    }
+
+	public String toString() {
+		if(of != null){
+		return "UserType/ Name: " + name + " TotalSize : "+ size + 
+			" BaseType : " + of + "\n";
+		//", size: " + size +"\n";
+		}else {
+			return "null\n";
+		}
+	}
+}
