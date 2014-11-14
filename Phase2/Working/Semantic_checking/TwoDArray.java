e6c1924490eb0a2f4de94d974279ef360a9929e1
Kyu <tkim371@gatech.edu>
2014-11-13 22:03:31 -0500

    uploading Semantic_checking Tiger.g

---
+++ Phase2/Working/Semantic_checking/TwoDArray.java
@@ -0,0 +1,28 @@
+//
+//  Type.java
+//  4240
+//
+//  Created by Sung Hye Jeon on 11/3/14.
+//  Copyright (c) 2014 Sung Hye Jeon. All rights reserved.
+//
+
+public class TwoDArray extends Type{
+
+	public Type of;
+	public int arraySize = 1; 
+	public int row = 1;
+	public int column = 1;
+	
+//s is name, w is size and p is type 
+	public TwoDArray(String s, int r, int c,  Type p) {	
+		super(s, c*r*p.size);
+		arraySize = r*c;
+		of = p;		
+		this.row = r;
+		this.column = c; 
+	}
+
+	public String toString(){
+		return "[" + row + "] " + "[" + column+"]" + of.toString();
+	}
+}
