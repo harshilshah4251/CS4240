aeb56d8d22fc6b6821f4ac55ccc84da4144ab173
Sung Hye Jeon <laurenj9744@gmail.com>
2014-12-03 08:01:21 -0500

    hi

--- Phase3/Working/ReadFile.java
+++ Phase3/Working/ReadFile.java
@@ -4,34 +4,34 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class ReadFile{
-	public static void main(String[] args) throws IOException {
-
-	int i = 0;
-		try(BufferedReader br = new BufferedReader(new FileReader(args[0]))) {
-		String sCurrentLine = "";
-		while(i < 8)  {
-			br.readLine();
-			i++;
-		}
-        while(!sCurrentLine.contains("END IR CODE")){
-            sCurrentLine = br.readLine();
-                
-            if((!sCurrentLine.contains("main:"))&&(!sCurrentLine.contains("END IR CODE"))){
-            System.out.println(sCurrentLine);
-            }
-        }
-	
-            
-            
-            
-        System.out.println("===========END===========");
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+    public static void main(String[] args) throws IOException {
         
-
-	}
+        int i = 0;
+        try(BufferedReader br = new BufferedReader(new FileReader(args[0]))) {
+            String sCurrentLine = "";
+            while(i < 8)  {
+                br.readLine();
+                i++;
+            }
+            while(!sCurrentLine.contains("END IR CODE")){
+                sCurrentLine = br.readLine();
+                
+                if((!sCurrentLine.contains("main:"))&&(!sCurrentLine.contains("END IR CODE"))){
+                    System.out.println(sCurrentLine);
+                }
+            }
+            
+            
+            
+            
+            System.out.println("===========END===========");
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        
+    }
 }
 
 
